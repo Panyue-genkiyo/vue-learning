8890c0928e9fc4987ce1fcd8d3fbd02060323534
panyue <90938238g@gmail.com>
2021-09-20 17:15:35 +0800

    单文件组件的写法示例

---
+++ 19_单文件组件/App.vue
@@ -0,0 +1,21 @@
+<template>
+   <div>
+     <School></School>
+     <Student></Student>
+   </div>
+</template>
+
+<script>
+import School from './School';
+import Student from "./Student";
+
+export default {
+  name: "App",
+  //汇总所有的组件
+  components:{
+    Student,
+    School
+  }
+}
+</script>
+
---
+++ 19_单文件组件/main.js
@@ -0,0 +1,11 @@
+//创建vm
+import App from './App';
+//如果文件
+new Vue({
+    el: '#root',
+    template:`<App></App>`,
+    components:{
+        App
+    }
+});
+
